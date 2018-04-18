8c38f5576a8a8264154eb82d119da757ac18cace
actondev <chris.actondev@gmail.com>
2018-04-18 03:01:59 +0300

    download dropbox on bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ ! -f ./_code/dropbox.tar.gz ]; then
+    echo "Downloading dropbox"
+    wget https://www.dropbox.com/download?plat=lnx.x86_64 -O ./_code/dropbox.tar.gz
+else
+    echo "Dropbox already downloaded, continuing with docker-compose"
+fi
+
 sudo docker-compose build
 sudo docker-compose up -d
